fde43c54cb8549824f991895561e3e29e2caad82
Martin <souzam@sonic.net>
2023-01-12 10:25:48 -0500

    Added error handling to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
-    # rescue_from 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable
 
     before_action :authorize
 
     private
     def authorize
         return render plain: "Unauthorized", status: :unauthorized unless session.include? :user_id
+    end
+
+    def render_not_found(exception)
+        render json: {errors: {exception.model: "Not found"}}, status: :not_found
+    end
+
+    def render_unprocessable(exception)
+        render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             render json: user, status: :created
         else
-            render json: {error: "Login error"}, status: :unauthorized
+            render json: {error: "Invalid email or password"}, status: :unauthorized
         end
     end
 
