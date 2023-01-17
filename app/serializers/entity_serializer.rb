8465f2914660cabb16c49deb21575b2b44406065
Martin <souzam@sonic.net>
2023-01-17 11:26:03 -0500

    Got images to show up on frontend

--- app/serializers/entity_serializer.rb
+++ app/serializers/entity_serializer.rb
@@ -5,9 +5,7 @@ class EntitySerializer < ActiveModel::Serializer
 
   def image
     if object.image.attached?
-      {
-        url: rails_blob_url(object.image)
-      }
+      rails_blob_url(object.image, only_path: true)
     end
   end
 end
