22411ad54b9ff067bd4d03662dec8375c62b8a50
Martin <souzam@sonic.net>
2023-01-18 09:24:31 -0500

    Fixed video embed preview image on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,16 @@
-import {Segment, Header, Embed} from 'semantic-ui-react'
+import {Segment, Header, Divider, Label, Embed} from 'semantic-ui-react'
 
 const Home = ({user}) => {
     return (
         <Segment>
             <Header size='large'>Welcome{user ? `, ${user.firstname} ${user.lastname}` : null}</Header>
             <Header size='huge' color='red'>We're ready to believe you!</Header>
-            <Embed autoplay={true} id='sRee26pfVzU' source='youtube'/>
+            <Divider />
+            <Label ribbon color='blue' size='large'>As Seen on TV!</Label>
+            <Embed
+                placeholder='https://img.youtube.com/vi/sRee26pfVzU/maxresdefault.jpg'
+                id='sRee26pfVzU'
+                source='youtube'/>
         </Segment>
     )
 }
